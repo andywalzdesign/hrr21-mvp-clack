56b5aefe0a02a1c238377c62d029c7335fcaa779
andywalzdesign <andywalzdesign@gmail.com>
2017-01-24 08:50:25 -0800

    added env var for mongodb

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mvp');
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));

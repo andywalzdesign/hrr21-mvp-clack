23a784ce8932aa4cf6273d44398508d202efbbfc
andywalzdesign <andywalzdesign@gmail.com>
2017-01-24 08:21:09 -0800

    added check for env port before specify port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 //var Users = require('./db/config.js');
 
 app.use('/node_modules', express.static(__dirname + '/node_modules'));

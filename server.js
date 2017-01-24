965a22680ece02a241ec79d8f90d44e83758bec1
andywalzdesign <andywalzdesign@gmail.com>
2017-01-24 08:10:05 -0800

    comment out parts that use the db due to connection issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-var Users = require('./db/config.js');
+//var Users = require('./db/config.js');
 
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/client', express.static(__dirname + '/client'));
@@ -17,32 +17,32 @@ app.get('/getclackin.html', function(req, res){
   res.sendFile('/client/getclackin.html', {root: __dirname});
 });
 
-app.get('/user', function(req, res){
-  var user = 'andy';
-  var stuff = [];
-  Users.find({}, function(err, users){
-    if(err){
-      console.log(err);
-    }
-    console.log('24', users);
-    stuff = users;
-    res.send(stuff);
-  });
-});
+// app.get('/user', function(req, res){
+//   var user = 'andy';
+//   var stuff = [];
+//   Users.find({}, function(err, users){
+//     if(err){
+//       console.log(err);
+//     }
+//     console.log('24', users);
+//     stuff = users;
+//     res.send(stuff);
+//   });
+// });
 
-app.post('/user', function(req, res){
-  //need to get data
-  var newUser = new Users({username: 'req.body.username', totalscore: 0});
-  console.log('34', newUser);
-  newUser.save(function(err, newUser){
-    if(err){
-      return console.log('err', err);
-    }
-    console.log(newUser);
-    res.send('HERRO post');
-    return newUser;
-  });
-});
+// app.post('/user', function(req, res){
+//   //need to get data
+//   var newUser = new Users({username: 'req.body.username', totalscore: 0});
+//   console.log('34', newUser);
+//   newUser.save(function(err, newUser){
+//     if(err){
+//       return console.log('err', err);
+//     }
+//     console.log(newUser);
+//     res.send('HERRO post');
+//     return newUser;
+//   });
+// });
 
 app.get('/*', function(req, res) {
   res.sendFile('./index.html');

f4656fec1ca91d88c1c721fbeca463757fbbfefb
andywalzdesign <andywalzdesign@gmail.com>
2017-01-24 07:50:10 -0800

    remove unneeded comments and useless code

--- db/config.js
+++ db/config.js
@@ -15,32 +15,3 @@ var userSchema = mongoose.Schema({
 var Users = mongoose.model('Users', userSchema);
 
 module.exports = Users;
-
-
-
-
-
-
-// var mongodb = require('mongodb');
-// var MongoClient = require('mongodb').MongoClient;
-// var url = 'mongodb://localhost:27017/mvp';
-
-// // Use connect method to connect to the server
-// MongoClient.connect(url, function(err, db) {
-//   console.log("Connected successfully to server");
-
-//   var collection = db.collection('Users');
-//   var user = {
-//     username: 'me',
-//     totalscore: 0
-//   };
-//   collection.insert(user, function(err, result){
-//     if(err){
-//       console.log(err);
-//     } else {
-//       console.log('added a user');
-//     }
-//   });
-// });
-
-// module.exports = MongoClient.connect;
